760ab281a0488402adae3190e5447bf0e5e06a2c
Bin Zhang <30135878@qq.com>
2015-08-07 11:58:31 +0800

    修复数据库端口号问题。

--- api.php
+++ api.php
@@ -2,14 +2,13 @@
 
 require_once ('./submodules/php-mysqli-database-class/MysqliDb.php');
 
-$db = new MysqliDb ('localhost:3306', 'root', 'Dsh12345', 'trafficpolice');
+$db = new MysqliDb ('localhost', 'root', 'Dsh12345', 'trafficpolice');
 $ip = $_SERVER['SERVER_ADDR'];
 if ($ip == '112.124.98.9') {
-	$db = new MysqliDb ('localhost:3306', 'root', 'Dsh12345', 'trafficpolice');
+	$db = new MysqliDb ('localhost', 'root', 'Dsh12345', 'trafficpolice');
 }
 
 $users = $db->get('user');
-var_dump($users);
 if ($db->count > 0) {
     foreach ($users as $user) { 
         print_r ($user);
