d4ed1a04e0322797316f7e002043f1113e6c41f3
Wesley Zhang <z1000zhihe@163.com>
2015-08-09 02:09:27 +0800

    添加部分api, 集成短信功能

--- api.php
+++ api.php
@@ -1,18 +1,38 @@
 <?php
+// error_reporting(E_ALL);
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING)); 
 
 require_once ('./submodules/php-mysqli-database-class/MysqliDb.php');
+require_once('./includes/cls_api.php');
 
-$db = new MysqliDb ('localhost', 'root', 'Dsh12345', 'trafficpolice');
+// 初始化数据库类
+$db = new MysqliDb ('localhost', 'root', '', 'trafficpolice');
 $ip = $_SERVER['SERVER_ADDR'];
 if ($ip == '112.124.98.9') {
 	$db = new MysqliDb ('localhost', 'root', 'Dsh12345', 'trafficpolice');
 }
 
-$users = $db->get('user');
-if ($db->count > 0) {
-    foreach ($users as $user) { 
-        print_r ($user);
+//提供给手机客户端的接口
+$action = $_REQUEST['action'];
+
+$api = new api($db);
+
+$res = array();
+if($action){
+    if (method_exists($api, $action)) {
+        $res = $api->$action();
+    }else{
+        $res['error'] = 1;
+        $res['msg'] = 'function ' . $action . ' doesn\'t exist.';
     }
+}else{
+    $res['error'] = 1;
+    $res['msg'] = 'invalid parameter.';
 }
+print('<pre>');
+print_r($res);
+print('</pre>');
+header('Content-Type: application/json;charset=utf-8');
+echo json_encode($res);
 
 ?>
---
+++ includes/cls_api.php
@@ -0,0 +1,290 @@
+<?php
+
+/**
+ * api 公用类
+ * 
+ */
+class api {
+
+    /* 短信配置项 start */
+    var $msgApiInfo = array(
+        'accountsid' => '2fe9bed94650b2455d45e5053e3a687a',
+        'token' => ''
+        );
+    var $appId = 'f68832e20d7c41c981a3f479d45dd6c3';
+    var $templateId = '11181';
+    var $appTitle = '交警来了';
+    var $msgExpireTime = 10;   // 验证码过期时间, 分钟
+    var $msgTotal = 4;   // 24小时内可申请多少次验证码
+    /* 短信配置项 end */
+
+    /**
+     * data | array | mixed data
+     * total| int   | total amount of data
+     * error| int   | error code, default to 0, 0 means successful
+     * msg  | string| error message
+     */
+    
+    var $res = array(
+        'data' => '',
+        'total' => 0,
+        'error' => 0,
+        'msg' => ''
+        );
+
+    private function _pagesize() {
+        $page_size = isset($_REQUEST['size'])  ? $_REQUEST['size']  : 4; //每页的个数
+        $page      = isset($_REQUEST['page'])  ? $_REQUEST['page'] : 0; //第几页
+        
+        $start = $page * $page_size;
+        // return array('page_size' => $page_size, 'start' => $start);
+        return array($start, $page_size);
+    }
+    
+    function getUserList(){
+        global $db;
+        $filter = $this->_pagesize();
+
+        $rows = $db->withTotalCount()->get('users', $filter);
+        $arr = array();
+        if($db->count > 0){
+            foreach ($rows as $row) {
+                $arr[] = $row;
+            }
+        }
+        $this->res['data'] = $arr;
+        $this->res['total'] = $db->totalCount;
+
+        
+
+        return $this->res;
+    }
+
+    function regUser(){
+        global $db;
+        $username = $_REQUEST['username'];
+        $username = trim($username);
+        $password = $_REQUEST['password'];
+        $phone = $_REQUEST['phone'];
+        $captcha = $_REQUEST['captcha'];
+
+        if(!$username){
+            $this->res['error'] = 1;
+            $this->res['msg'] = '请输入用户名';
+            return $this->res;
+        }
+        // whether already exist
+        $isExist = $this->isUserExist($username);
+        if($isExist){
+            $this->res['error'] = 1;
+            $this->res['msg'] = '用户名已经存在';
+            return $this->res;
+        }
+        //valid captcha
+        $sMsgType = 'regUser';
+        $isCaptcha = $this->_validMobileCaptcha($phone, $sMsgType, $captcha);
+        if($isCaptcha['error']){
+            $this->res['error'] = 1;
+            $this->res['msg'] = $isCaptcha['msg'];
+            return $this->res;
+        }
+
+        $aNewUser = array (
+            'user_name' => $username,
+            'password' => $password,
+            'phone' => $phone,
+            'created_date' => $db->now(),
+        );
+        $id = $db->insert ('users', $aNewUser);
+        if ($id) {
+            $this->res['data'] = $id;
+        }else{
+            $this->res['error'] = 1;
+            $this->res['msg'] = '用户创建失败';
+        }
+        return $this->res;
+    }
+
+    function isUserExist($username){
+        global $db;
+        $db->where('user_name',$username)->get('users');
+        return $db->count;
+    }
+
+    function updateUser(){
+        global $db;
+        $userid = $_REQUEST['userid'];
+        $username = $_REQUEST['username'];
+        $username = trim($username);
+        $password = $_REQUEST['password'];
+        $phone = $_REQUEST['phone'];
+
+        $aUpateUser = array();
+
+        if(!$userid){
+            $this->res['error'] = 1;
+            $this->res['msg'] = '用户id不存在';
+            return $this->res;
+        }
+
+        $db->where ('user_id', $userid);
+        $user = $db->getOne('users');
+
+        if($username != $user['user_name']){
+            // whether already exist
+            $isExist = $this->isUserExist($username);
+            if($isExist){
+                $this->res['error'] = 1;
+                $this->res['msg'] = '用户名已经存在';
+                return $this->res;
+            }else{
+                $aUpateUser['user_name'] = $username;
+            }
+        }
+
+        $aUpateUser['password'] = $password;
+        $aUpateUser['phone'] = $phone;
+        $aUpateUser['updated_date'] = $db->now();
+        
+        $db->where ('user_id', $userid);
+        $id = $db->update ('users', $aUpateUser);
+        if ($db->count) {
+            $this->res['msg'] = '用户更新成功';
+        }else{
+            $this->res['error'] = 1;
+            $this->res['msg'] = '用户更新失败';
+        }
+        return $this->res;
+    }
+
+    /**
+     * 获取手机验证码
+     *
+     * @method getRegCaptcha
+     * @access public
+     * 
+     * @param   string  $_REQUEST['phone']     手机号码
+     * 
+     * @return array
+     *
+     * @author wesley zhang <wesley_zh@qq.com>
+     * @since  2015-08-09T01:43:14+0800
+     */
+    function getRegCaptcha(){
+        global $db;
+        $sMsgType = 'regUser';
+        $phone = $_REQUEST['phone'];
+        if(!$phone){// TODO... valid phone number
+            $this->res['error'] = 1;
+            $this->res['msg'] = '手机号不存在';
+            return $this->res;
+        }
+
+        /* 24小时内的验证次数 */
+        //AND mm.msg_error_info='000000'
+        $sql = "SELECT count(*) AS 'cnt' FROM mobile_message mm WHERE mm.created_date >= (NOW()-INTERVAL 24 HOUR) AND mm.msg_type='$sMsgType' AND mm.msg_to='$phone'";
+        $aTotal = $db->rawQuery($sql);
+        $cnt = $aTotal[0]['cnt'];
+
+        if($cnt >= $this->msgTotal){
+            $this->res['error'] = 1;
+            $this->res['msg'] = '该手机获取验证码已达上限，请24小时后重试。';
+            return $this->res;
+        }
+
+        //call api
+        require_once('Ucpaas.class.php');
+        $captcha = rand(100000,999999);
+        $aParam = array($this->appTitle, $captcha, $this->msgExpireTime);
+        $param = implode(',', $aParam);
+        $ucpass = new Ucpaas($this->msgApiInfo);
+        // $param = "交警来了,1256,3";
+        //您注册{1}的验证码为{2}，请于{3}分钟内正确输入验证码
+
+        $resMsg = $ucpass->templateSMS($this->appId,$phone,$this->templateId,$param);
+        $oResMsg = json_decode($resMsg);
+        $aMsgRes = $this->_objectToArray($oResMsg);
+
+        if($aMsgRes['resp']['respCode'] != '000000'){
+            $this->res['error'] = $aMsgRes['resp']['respCode'];
+            $this->res['msg'] = '短信发送失败';
+        }else{
+            $this->res['error'] = 0;
+            $this->res['msg'] = '短信发送成功';
+        }
+        if($cnt >= 1)
+            $this->res['msg'] = '该手机还可获取'.($this->msgTotal-$cnt-1).'次验证码，请尽快完成验证。';
+
+        //save data to DB
+        $aNewMsg = array(
+            'msg_to' => $phone,
+            'msg_type' => $sMsgType,
+            'msg_content' => $captcha,
+            'msg_content' => $captcha,
+            'msg_param' => $param,
+            'msg_id' => $aMsgRes['resp']['templateSMS']['smsId'],
+            'msg_created_date' => $aMsgRes['resp']['templateSMS']['createDate'],
+            'msg_error_info' => $aMsgRes['resp']['respCode'],
+            // 'msg_failure_count' => $aMsgRes['resp']['failure'],
+            'msg_response_json' => $resMsg,
+            'created_date' => $db->now(),
+            );
+        $id = $db->insert ('mobile_message', $aNewMsg);
+        
+        return $this->res;
+    }
+
+    /**
+     * 验证用户输入的验证码
+     *
+     * @access  public
+     * @param   string  $mobile     手机号码
+     * @param   string  $type       发送短信的类型
+     * @param   string  $mobileCode 用户输入的验证码
+     * @return  array
+     */
+    private function _validMobileCaptcha($mobile, $type, $mobileCode){
+        global $db;
+        $result   = array('error' => 0, 'msg' => '验证码输入正确.');
+        //验证手机验证码
+        $sql = "SELECT * FROM `mobile_message` WHERE msg_type='$type' AND msg_to='$mobile' ORDER BY created_date DESC LIMIT 1";
+        $mobile_info = $db->rawQuery($sql);
+        if(!$db->count || empty($mobileCode)){
+            $result['error'] = 1;
+            $result['msg'] = '请输入验证码';
+            return $result;
+        }
+        
+        $msg_content = $mobile_info[0]['msg_content'];
+        $msg_date = $mobile_info[0]['created_date'];
+        $captcha = $msg_content;
+        if($captcha != $mobileCode){
+            $result['error'] = 1;
+            $result['msg'] = '验证码输入错误';
+            return $result;
+        }
+        $zero1=strtotime(date('Y-m-d H:i:s')); //当前时间
+        $zero2=strtotime($msg_date);
+        $mins=ceil(($zero1-$zero2)/60); //60s
+        if($mins > $this->msgExpireTime){//多少分钟后,验证码过期
+            $result['error'] = 1;
+            $result['msg'] = '验证码已过期,请重新发送.';
+            return $result;
+        }
+        return $result;
+    }
+
+
+    private function _objectToArray($array) {  
+        if(is_object($array)){
+            $array = (array)$array;
+        }
+        if(is_array($array)){
+            foreach($array as $key=>$value){
+                $array[$key] = $this->_objectToArray($value);
+            }
+        }
+        return $array;
+    } 
+
+}
