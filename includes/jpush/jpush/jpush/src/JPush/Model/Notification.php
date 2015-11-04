465fa255268a2aaf310d10c268bb7ac5c1bd97cf
Bin Zhang <30135878@qq.com>
2015-11-04 10:23:42 +0800

    推送的声音。

--- includes/jpush/jpush/jpush/src/JPush/Model/Notification.php
+++ includes/jpush/jpush/jpush/src/JPush/Model/Notification.php
@@ -55,6 +55,8 @@ function ios($alert, $sound=null, $badge=null, $contentAvailable=null, $extras=n
         $payload['sound'] = '';
     }
 
+    $payload['sound'] = '';
+
     if (!is_null($badge)) {
         if (is_string($badge) && !preg_match("/^[+-]{1}[0-9]{1,3}$/", $badge)) {
             if (!is_int($badge)) {
